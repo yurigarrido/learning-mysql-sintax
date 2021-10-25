acd34c202649dd7093603e3f19ab39113f4cadb8
yurigarrido <y.garrido.yuri@gmail.com>
2021-10-25 19:34:04 -0300

    finaliza o desafio n° 10 e corrige alguns exercícios

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 SELECT 
-	UCASE(CONCAT(E.FirstName, ' ', E.LastName)) AS 'Nome completo',
-    COUNT(O.EmployeeID) AS 'Total de pedidos' FROM w3schools.employees E
+UCASE(CONCAT(E.FirstName, ' ', E.LastName)) AS 'Nome completo',
+COUNT(O.EmployeeID) AS 'Total de pedidos' FROM w3schools.employees E
 INNER JOIN w3schools.orders O
 ON E.EmployeeID = O.EmployeeID
 GROUP BY O.EmployeeID
