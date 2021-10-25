e05429b95141956c084935e42c6bddc5b3069b31
yurigarrido <y.garrido.yuri@gmail.com>
2021-10-25 17:25:14 -0300

    finaliza o desafio n° 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT 
+	UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
+    JH.START_DATE AS 'Data de início',
+    E.SALARY AS 'Salário'
+FROM hr.job_history JH
+INNER JOIN hr.employees E
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE MONTH(START_DATE) BETWEEN 1 AND 3;
