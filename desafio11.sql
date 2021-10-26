9e77e928ac8f976e979587185f722db5b5a60576
yurigarrido <y.garrido.yuri@gmail.com>
2021-10-26 10:26:23 -0300

    finaliza o exercicio 11 e corrige o 2 e 5

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT 
+	C.ContactName AS 'Nome',
+    C.Country AS 'País',
+    ( SELECT COUNT(C2.Country) - 1 
+		FROM w3schools.customers C2
+        WHERE C2.Country = C.Country) AS 'Número de compatriotas'
+FROM w3schools.customers C
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome` ASC;
