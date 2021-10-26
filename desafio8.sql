6b75232c08175c92319415a33e99c409b9bae14b
yurigarrido <y.garrido.yuri@gmail.com>
2021-10-26 13:13:05 -0300

    ajusta as querys no pádrão do code climate -desafio 8-

--- desafio8.sql
+++ desafio8.sql
@@ -2,13 +2,10 @@ SELECT
 C.ContactName AS 'Nome de contato',
 S.ShipperName AS 'Empresa que fez o envio',
 O.orderDate AS 'Data do pedido'
-FROM
-w3schools.orders O
-INNER JOIN
-w3schools.shippers S
+FROM w3schools.orders O
+INNER JOIN w3schools.shippers S
 ON S.ShipperId = O.ShipperId
-INNER JOIN 
-w3schools.customers C
+INNER JOIN w3schools.customers C
 ON O.CustomerID = C.CustomerID 
 WHERE S.ShipperName = 'Speedy Express' OR S.ShipperName = 'United Package'
-ORDER BY `Nome de contato` ASC ,`Empresa que fez o envio` ASC;
+ORDER BY `Nome de contato` ASC ,`Empresa que fez o envio`, `Data do pedido`;
